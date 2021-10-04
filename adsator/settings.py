e2cfc4a970bbd1e432b765e19b4583b3fad5dde8
Wesley Duarte <wesleydmuniz@outlook.com>
2021-10-04 01:43:44 -0300

    integrate frontend app

--- adsator/settings.py
+++ adsator/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'advertisements',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
--- adsator/urls.py
+++ adsator/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('advertisements.urls'))
 ]
